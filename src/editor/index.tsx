bb4711c4c7ad9d3caad2db914dd640c5d0236a8b
Raaj <raaj.ahab@gmail.com>
2025-05-07 19:57:06 +0000

    adds renderer components

--- src/editor/index.tsx
+++ src/editor/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { Puck } from "@measured/puck";
 import "@measured/puck/puck.css";
 
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { Render  } from "@measured/puck"
 import components from "../components"
 
